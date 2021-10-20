7f98bfd794ce38d6d26a83fd20309687cb697956
Alexander Britcliffe <alexanderbritcliffe@gmail.com>
2021-10-20 13:35:19 -0400

    set up basic backend functionality

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
+import axios from 'axios';
 
 const ProductScreen = ({ match }) => {
-  const product = products.find(p => p._id === match.params.id)
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
+
+      setProduct(data)
+    }
+
+    fetchProduct()
+  }, [match])
 
   return (
      <>
@@ -54,16 +64,19 @@ const ProductScreen = ({ match }) => {
                   Status:
                 </Col>
                 <Col>
-                  <strong>${product.countInStock} > 0 'In Stock' : 'Out Of Stock'}</strong>
+                  {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
                 </Col>
               </Row>
             </ListGroup.Item>
             <ListGroup.Item>
-              <Button className='btn-block' type='button'>
+              <Button className='btn-block'
+                      type='button'
+                      disabled={product.countInStock === 0}>
                 Add To Cart
               </Button>
             </ListGroup.Item>
           </ListGroup>
+        </Card>
       </Col>
     </Row>
   </>
